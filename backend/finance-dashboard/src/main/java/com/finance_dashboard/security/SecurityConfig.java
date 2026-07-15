283adfa7b616d3f45a9dc131670811510fd64fe0
Adam <ajlarson0731@gmail.com>
2026-07-15 00:55:33 -0400

    Added some comments to SecurityConfig.java.

--- backend/finance-dashboard/src/main/java/com/finance_dashboard/security/SecurityConfig.java
+++ backend/finance-dashboard/src/main/java/com/finance_dashboard/security/SecurityConfig.java
@@ -16,7 +16,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import com.finance_dashboard.authentication.CustomUserDetailsService;
 
+// Tells Spring the class contains bean definitions
 @Configuration
+// Activates Spring Security's web security support
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -34,7 +36,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http, DaoAuthenticationProvider authenticationProvider)
             throws Exception {
         http
+                // Disable CSRF protection since API uses tokens
                 .csrf(csrf -> csrf.disable())
+                // Allows anyone to visit registration or login endpoints
+                // But makes any other endpoint require a valid JWT
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/authentication/login",
@@ -42,11 +47,17 @@ public class SecurityConfig {
                         .permitAll()
                         .anyRequest()
                         .authenticated())
+                // Tells Spring Securiy to use CustomUserDetailsService to look up credentials
                 .userDetailsService(userDetailsService)
+                // Tells server not to remember user between requests, every request must contain JWT
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Tells application to check for JWT first before login filter
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                // Disables cookie-based browser login form
                 .formLogin(form -> form.disable())
+                // Enables basic HTTP authentication
                 .httpBasic(Customizer.withDefaults())
+                // Registers DaoAuthenticationProvider bean to compare raw password from login attempt to hashed password
                 .authenticationProvider(authenticationProvider);
 
         return http.build();
