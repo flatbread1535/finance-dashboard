081729b8f52573e281b8378d9cd4c2d0e1b82eb5
Adam <ajlarson0731@gmail.com>
2026-07-15 00:04:30 -0400

    Added some comments and changed to UsernameNotFoundException instead of ResourceNotFoundException in the loadUserByUsername method.

--- backend/finance-dashboard/src/main/java/com/finance_dashboard/authentication/CustomUserDetailsService.java
+++ backend/finance-dashboard/src/main/java/com/finance_dashboard/authentication/CustomUserDetailsService.java
@@ -1,11 +1,11 @@
 package com.finance_dashboard.authentication;
 
-import com.finance_dashboard.ResourceNotFoundException;
 import com.finance_dashboard.accounts.Account;
 import com.finance_dashboard.accounts.AccountRepository;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,13 +18,16 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
+    // Returns a UserDetails object Spring understands, fetches Account security data from database
     public UserDetails loadUserByUsername(String username) {
         Account account = accountRepository.findByUsername(username)
-                .orElseThrow(() -> new ResourceNotFoundException("Could not find account."));
+                .orElseThrow(() -> new UsernameNotFoundException("Could not find account."));
 
+        // User class translates database fields into UserDetails object
+        // Maps username to security context
         return User.withUsername(account.getUsername())
                 .password(account.getHashPassword())
-                .roles("USER")
+                .roles(account.getRole().name())
                 .build();
-    }
+    } 
 }
