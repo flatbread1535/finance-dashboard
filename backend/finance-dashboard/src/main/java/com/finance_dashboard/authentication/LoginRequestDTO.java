62f6ef2d2426353b26672095ec658a3221b61bfe
Adam <ajlarson0731@gmail.com>
2026-07-14 23:57:07 -0400

    Added validation to the login request DTO.

--- backend/finance-dashboard/src/main/java/com/finance_dashboard/authentication/LoginRequestDTO.java
+++ backend/finance-dashboard/src/main/java/com/finance_dashboard/authentication/LoginRequestDTO.java
@@ -1,13 +1,16 @@
 package com.finance_dashboard.authentication;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequestDTO(
     
-    @NotBlank(message = "Username cannot be blank.")
+    @NotBlank(message = "Username cannot be blank.")   
+    @Size(max = 20, message = "Username cannot be more than 20 characters.")
     String username,
 
     @NotBlank(message = "Password cannot be blank.")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters.")
     String password
 
 ) {}
