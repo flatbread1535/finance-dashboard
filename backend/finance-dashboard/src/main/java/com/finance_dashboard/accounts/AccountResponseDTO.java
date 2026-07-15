6aeaa41b3e58cbccd3f9a03c2aa96ca8ebfa48ae
Adam <ajlarson0731@gmail.com>
2026-07-14 23:11:01 -0400

    Updated the account response DTO to reflect changes to the accounts table such as adding components for Role, profile picture URL, dark mode UI setting, account creation timestamp, and login timestamp.

--- backend/finance-dashboard/src/main/java/com/finance_dashboard/accounts/AccountResponseDTO.java
+++ backend/finance-dashboard/src/main/java/com/finance_dashboard/accounts/AccountResponseDTO.java
@@ -1,8 +1,15 @@
 package com.finance_dashboard.accounts;
 
+import java.time.LocalDateTime;
+
 public record AccountResponseDTO(
-    Long accountId,
-    String username,
-    String email,
-    String phoneNumber
-) {}
+                Long accountId,
+                Role role,
+                String username,
+                String email,
+                String phoneNumber,
+                String profilePictureUrl,
+                Boolean darkModeEnabled,
+                LocalDateTime timeCreated,
+                LocalDateTime lastLoginTime) {
+}
