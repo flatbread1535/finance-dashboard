3045ea490d802ba3830c05c1ed670333a8aabca1
Adam <ajlarson0731@gmail.com>
2026-07-14 23:09:10 -0400

    Updated validation for the Account Request DTO.

--- backend/finance-dashboard/src/main/java/com/finance_dashboard/accounts/AccountRequestDTO.java
+++ backend/finance-dashboard/src/main/java/com/finance_dashboard/accounts/AccountRequestDTO.java
@@ -9,15 +9,17 @@ public record AccountRequestDTO(
     String username,
 
     @NotBlank(message = "Email cannot be blank.")
+    @Size(max = 100, message = "Email cannot be more than 100 characters.")
     @Email(message = "Invalid email format.")
     String email,
-
-    @NotBlank(message = "Password cannot be blank.")
-    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters.")
-    String password,
     
     // TODO: Phone number custom validation
     @NotBlank(message = "Phone number cannot be blank.")
-    String phoneNumber
+    @Size(max = 20, message = "Phone number cannot be more than 20 characters.")
+    String phoneNumber,
+
+    @NotBlank(message = "Password cannot be blank.")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters.")
+    String password
 
 ) {}
