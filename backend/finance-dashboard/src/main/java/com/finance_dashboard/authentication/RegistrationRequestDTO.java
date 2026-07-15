adc938690535cde9363b0e3c4289644501f90471
Adam <ajlarson0731@gmail.com>
2026-07-14 23:56:34 -0400

    Added validation to the registration request DTO.

--- backend/finance-dashboard/src/main/java/com/finance_dashboard/authentication/RegistrationRequestDTO.java
+++ backend/finance-dashboard/src/main/java/com/finance_dashboard/authentication/RegistrationRequestDTO.java
@@ -7,17 +7,20 @@ import jakarta.validation.constraints.Size;
 public record RegistrationRequestDTO(
     
     @NotBlank(message = "Username cannot be blank.")
+    @Size(max = 20, message = "Username cannot be more than 20 characters.")
     String username,
 
     @NotBlank(message = "Email cannot be blank.")
+    @Size(max = 50, message = "Email cannot be more than 50 characters.")
     @Email(message = "Invalid email format.")
     String email,
 
+    @NotBlank(message = "Phone number cannot be blank.")
+    @Size(max = 20, message = "Phone number cannot be more than 20 characters.")
+    String phoneNumber,
+
     @NotBlank(message = "Password cannot be blank.")
     @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters.")
-    String password,
-
-    @NotBlank(message = "Phone number cannot be blank.")
-    String phoneNumber
+    String password
     
 ) {}
