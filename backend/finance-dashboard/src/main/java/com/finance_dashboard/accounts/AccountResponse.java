f2d506481b2ae836e3963364d125a15ce106c7f4
Adam <ajlarson0731@gmail.com>
2026-07-18 00:01:19 -0400

    Renamed AccountResponseDTO to just AccountResponse.

---
+++ backend/finance-dashboard/src/main/java/com/finance_dashboard/accounts/AccountResponse.java
@@ -0,0 +1,15 @@
+package com.finance_dashboard.accounts;
+
+import java.time.LocalDateTime;
+
+public record AccountResponse(
+                Long accountId,
+                Role role,
+                String username,
+                String email,
+                String phoneNumber,
+                String profilePictureUrl,
+                Boolean darkModeEnabled,
+                LocalDateTime timeCreated,
+                LocalDateTime lastLoginTime) {
+}
