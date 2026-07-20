6177fb11435b681758ee210086babe77978887f8
Adam <ajlarson0731@gmail.com>
2026-07-20 00:13:15 -0400

    Remove deleted files.

--- backend/finance-dashboard/src/main/java/com/finance_dashboard/accounts/AccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.finance_dashboard.accounts;
-
-public class AccountNotFoundException extends RuntimeException {
-    public AccountNotFoundException(String message) {
-        super(message);
-    }
-}
--- backend/finance-dashboard/src/main/java/com/finance_dashboard/users/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.finance_dashboard.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
