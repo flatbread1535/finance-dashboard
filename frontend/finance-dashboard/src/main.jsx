63b92cefc8432ef785ff3da53f61781394161a76
Adam <ajlarson0731@gmail.com>
2026-07-22 01:02:01 -0400

    Added initial React Router setup.

--- frontend/finance-dashboard/src/main.jsx
+++ frontend/finance-dashboard/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import './index.css'
 import App from './App.jsx'
 
+// Creates the configuration for a router by passing arguments in the form of
+// an array of routes
+const router = createBrowserRouter([
+  {
+    // These keys are mandatory, path and element to be rendered
+    path: "/",
+    element: <App />
+  }
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Configuration is rendered in by passing it to the RouterProvider component */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
