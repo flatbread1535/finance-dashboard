ae0f49af1c5172979b957a6c7ab19acb907c787f
Adam <ajlarson0731@gmail.com>
2026-07-28 01:04:43 -0400

    Imported routes to main file.

--- frontend/finance-dashboard/src/main.jsx
+++ frontend/finance-dashboard/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import routes from "./routes";
+import routes from "./routes.jsx";
 
 // Creates the configuration for a router by passing arguments in the form of
 // an array of routes
