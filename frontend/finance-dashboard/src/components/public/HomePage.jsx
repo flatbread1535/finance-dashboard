5bf82c46a59c1fe2daf5f17cb8f8347dd75b1bf9
Adam <ajlarson0731@gmail.com>
2026-08-02 21:04:00 -0400

    Built out home page component and added styling and a website demo screenshot.

--- frontend/finance-dashboard/src/components/public/HomePage.jsx
+++ frontend/finance-dashboard/src/components/public/HomePage.jsx
@@ -1,7 +1,27 @@
+import demoImage from "../../assets/images/web-demo.png";
 import "../../styles/public/PublicLayout.css";
+import "../../styles/public/HomePage.css";
 
 const HomePage = () => {
-  return <></>;
+  return (
+    <main className="home-page">
+      <section className="info">
+        <p>A personal finance dashboard to manage college expenses</p>
+        <ul>
+          <li>Gives important sumaries</li>
+          <li>Displays interactive charts</li>
+          <li>Tracks your transactions</li>
+        </ul>
+      </section>
+      <section className="demo-screenshot-container">
+        <img
+          src={demoImage}
+          className="demo-screenshot"
+          alt="Website demo image with view of transaction page table chart"
+        ></img>
+      </section>
+    </main>
+  );
 };
 
 export default HomePage;
