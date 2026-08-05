51fe0fa60ee6dd66b809f31507a3f2385c275c06
Adam <ajlarson0731@gmail.com>
2026-08-04 23:30:53 -0400

    Built a basic settings component that only handles logging out.

--- frontend/finance-dashboard/src/components/settings/Settings.jsx
+++ frontend/finance-dashboard/src/components/settings/Settings.jsx
@@ -1,5 +1,37 @@
+import { useNavigate } from "react-router";
+import { GoBackIcon } from "../../assets/svgs/SettingsIcons";
+import "../../styles/settings/Settings.css";
+
 const Settings = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  const handleBackBtn = () => {
+    navigate(-1);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  return (
+    <main className="settings">
+      <section className="back-btn-container">
+        <button type="button" onClick={handleBackBtn}>
+          <GoBackIcon />
+          Go Back
+        </button>
+      </section>
+      <section className="logout-container">
+        <div>
+          <p>Do you wish to log out?</p>
+          <button type="button" onClick={handleLogout}>
+            Log Out
+          </button>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Settings;
