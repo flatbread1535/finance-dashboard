c3d7b63b71af588193e6cf52f51b2bce431eddab
Adam <ajlarson0731@gmail.com>
2026-08-07 17:02:58 -0400

    Linted entry point file and added Javadoc.

---
+++ backend/finance-dashboard/src/main/java/com/financedashboard/FinanceDashboardApplication.java
@@ -0,0 +1,21 @@
+package com.financedashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point for application.
+ */
+@SpringBootApplication
+public class FinanceDashboardApplication {
+
+  /**
+   * Main method used to run Spring Boot application.
+   *
+   * @param args command-line arguments passed to the application
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(FinanceDashboardApplication.class, args);
+  }
+}
+ 
